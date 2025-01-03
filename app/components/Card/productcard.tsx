dd9a665b674b7c3735b36558d2a6505b65c7998e
LetiziaNunes <160158100+LetiziaNunes@users.noreply.github.com>
2025-01-03 22:59:04 +0000

    erro card

---
+++ app/components/Card/productcard.tsx
@@ -0,0 +1,12 @@
+
+import { Product } from '../../models/interfaces'
+import React from 'react'
+
+/*
+export default function ProductCard((id, title, price, description, category, image, rating ) : Product){
+return <article>
+</article>
+}
+
+
+*/
